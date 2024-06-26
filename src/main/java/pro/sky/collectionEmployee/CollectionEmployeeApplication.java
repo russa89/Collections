b172054a54de478ed42a9868173bf10bdc5afc77
russa89 <natast2189@gmail.com>
2024-06-26 12:12:53 +0300

    initial commit

---
+++ src/main/java/pro/sky/collectionEmployee/CollectionEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionEmployeeApplication.class, args);
+	}
+
+}
